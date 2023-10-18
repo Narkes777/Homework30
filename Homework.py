f6a0aceca8b4213210b6fe42472ebc37295abf02
Narkes777 <62197668+Narkes777@users.noreply.github.com>
2023-10-18 16:35:48 +0600

    Add files via upload

---
+++ Homework.py
@@ -0,0 +1,12 @@
+
+rook_x = int(input())
+rook_y = int(input())
+
+figure_x = int(input())
+figure_y = int(input())
+
+if rook_x == figure_x or rook_y == figure_y:
+    print("YES")
+else:
+    print("NO")
+
